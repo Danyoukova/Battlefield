fddbb7a6652ff7346d092a5208273e81407f747c
Nataliya <nataliya.danyoukova@student.howest.be>
2020-12-28 21:36:07 +0100

    Add SimpleBattlefield service

---
+++ Pra.Battlefield/Pra.Battlefield.Core/Services/SimpleBattlefield.cs
@@ -0,0 +1,88 @@
+﻿using Pra.Battlefield.Core.Entities.Players;
+using Pra.Battlefield.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pra.Battlefield.Core.Services
+{
+    public class SimpleBattlefield : IBattlefield
+    {
+        public List<IPlayer> Players { get; }
+        Random random = new Random();
+
+
+        public void AddGeneratedPlayer()
+        {
+            Players.Add(new SimplePlayer());
+        }
+
+        public virtual void Attack()
+        {
+           //List<IPlayer> playersAlive = new List<IPlayer>();
+           //foreach(IPlayer player in Players)
+           // { if (player.Health>0)
+           //     {
+           //         playersAlive.Add(player);
+           //     }
+            
+           // }
+            foreach (IPlayer player in Players)
+            {
+                if(player is SimplePlayer)
+                {
+                    int damage = random.Next(0, 11);
+                    player.TakeDamage(damage);
+                }
+              if(player.Health<0)
+                {
+                   
+                    player.IsAlive = false;
+                }
+            }
+
+        }
+        public int NumberAlive()
+        {
+            int numberAlive = 0;
+            foreach(IPlayer player in Players)
+            {
+                if(player.Health>0)
+                {
+                    numberAlive++;
+                    
+                }
+            }
+            return numberAlive;
+           
+        }
+
+        public List<IPlayer> GetAlivePlayers()
+        {
+            //List<IPlayer> players = new List<IPlayer>();
+           
+            foreach (IPlayer player in Players)
+            {
+                if (player.Health > 0 )
+                {
+                    player.IsAlive = true;
+                    Players.Add(player);
+                   
+                }
+                else
+                {
+                    player.IsAlive = false;
+                }
+
+            }
+           
+            return Players;
+        }
+
+        public SimpleBattlefield()
+        {
+            Players = new List<IPlayer>();
+        }
+    }
+}
