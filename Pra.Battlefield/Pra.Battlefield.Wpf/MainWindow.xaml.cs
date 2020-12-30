4338a130242e63ab7c28552fc9c6ecb9d3b0cdd0
Nataliya <nataliya.danyoukova@student.howest.be>
2020-12-30 08:54:08 +0100

    Add wpf components

--- Pra.Battlefield/Pra.Battlefield.Wpf/MainWindow.xaml.cs
+++ Pra.Battlefield/Pra.Battlefield.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Pra.Battlefield.Core.Entities.Players;
+using Pra.Battlefield.Core.Interfaces;
+using Pra.Battlefield.Core.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,25 +29,125 @@ namespace Pra.Battlefield.Wpf
             InitializeComponent();
         }
 
+        SimpleBattlefield simpleBattlefield;
+        AdvancedBattlefield advancedBattlefield;
+
+        List<IPlayer> players = new List<IPlayer>();
+
+        public void DefaultView()
+        {
+            btnNewGame.IsEnabled = true;
+            grpBattlefield.IsEnabled = false;
+            chkSimpleGame.IsChecked = true;
+            chkSimpleGame.IsEnabled = true;
+            lblPlayers.Content = "";
+            lblPlayersAlive.Content = "";
+        }
+        public void StartGame()
+        {
+            grpBattlefield.IsEnabled = true;
+            btnNewGame.IsEnabled = false;
+            chkSimpleGame.IsEnabled = false;
+        }
+        public int CountAlive()
+        {
+            foreach (IPlayer player in players)
+            {
+                if (player.Health > 0)
+                {
+                    lblPlayersAlive.Content = players.Count;
+                }
+            }
+            return players.Count;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DefaultView();
+           
+            
+        }
+
         private void BtnNewGame_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
+            StartGame();
+            if (chkSimpleGame.IsChecked == true)
+            {
+                simpleBattlefield = new SimpleBattlefield();
+            }
+            else
+            {
+                advancedBattlefield = new AdvancedBattlefield();
+            }
 
-        private void BtnQuitGame_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
 
+        }
         private void BtnAddPlayer_Click(object sender, RoutedEventArgs e)
         {
+
+            lstPlayers.ItemsSource = null;
+            if(chkSimpleGame.IsChecked==true)
+            {
+                simpleBattlefield.AddGeneratedPlayer();
+                lstPlayers.ItemsSource = simpleBattlefield.Players;
+                lblPlayers.Content = simpleBattlefield.Players.Count();
+                lblPlayersAlive.Content = simpleBattlefield.NumberAlive();
+            }
+            else
+            {
+                advancedBattlefield.AddGeneratedPlayer();
+                lstPlayers.ItemsSource = advancedBattlefield.Players;
+                lblPlayers.Content = advancedBattlefield.Players.Count();
+                lblPlayersAlive.Content = advancedBattlefield.NumberAlive();
+            }
             
+           
         }
 
         private void BtnAttack_Click(object sender, RoutedEventArgs e)
         {
 
+        if (chkSimpleGame.IsChecked == true)
+            {
+                if (simpleBattlefield.NumberAlive() >= 2)
+                {
+                    simpleBattlefield.Attack();
+                    lblPlayersAlive.Content = simpleBattlefield.NumberAlive();
+                }
+                else
+                {
+                    MessageBox.Show("Voeg nog spelers toe.");
+                }
+            }
+            else
+            {
+                if(advancedBattlefield.NumberAlive()>=2)
+                {
+                    advancedBattlefield.Attack();
+                    lblPlayersAlive.Content = advancedBattlefield.NumberAlive();
+                }
+                else
+                {
+                    MessageBox.Show("Voeg nog spelers toe.");
+                }
+            }
+              
+            
+           lstPlayers.Items.Refresh();
+            
+         
         }
 
+
+        private void BtnQuitGame_Click(object sender, RoutedEventArgs e)
+        {
+            players.Clear();
+            lstPlayers.ItemsSource = null;
+            lstPlayers.Items.Refresh();
+            DefaultView();
+        }
+
+       
+        
     }
 }
